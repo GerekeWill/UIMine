8b7cc42ec7d7aa9698e703771bd411bbff233c47
GerekeWill <gerekewill@gmail.com>
2024-12-08 12:27:36 -0500

    Add files via upload

---
+++ UIMineSound/Patches/MinePatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using static System.Net.Mime.MediaTypeNames;
+using UnityEngine;
+
+
+namespace UIMineSound.Patches
+{
+    [HarmonyPatch(typeof(Landmine))]
+    internal class MinePatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void ReplaceSound(Landmine __instance, ref AudioSource ___mineAudio, ref AudioSource ___mineFarAudio, ref Animator ___mineAnimator)
+        {
+            if (UIMineSound.silenceMines.Value.Equals(true))
+            {
+                ___mineAudio.enabled = false;
+                ___mineFarAudio.enabled = false;
+                ___mineAnimator.speed = 0f;
+            }
+            __instance.mineTrigger = UIMineSound.UI[0];
+        }
+        [HarmonyPatch("PressMineServerRpc")]
+        [HarmonyPrefix]
+        static void TurnBackOn(ref AudioSource ___mineAudio, ref AudioSource ___mineFarAudio, ref Animator ___mineAnimator)
+        {
+            ___mineAnimator.speed = 1f;
+            ___mineAudio.enabled = true;
+            ___mineAudio.pitch = 1f;
+            ___mineFarAudio.enabled = true;
+        }
+    }
+}
---
+++ UIMineSound/UIMineSound.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using System.Reflection;
+using BepInEx.Configuration;
+
+namespace UIMineSound
+{
+    [BepInPlugin("KurisuBot.UIMineSound", "UIMineSound", "1.0.0")]
+    public class UIMineSound : BaseUnityPlugin
+    {
+        public static UIMineSound instance;
+        public static ConfigEntry<bool> silenceMines;
+        internal ManualLogSource mls;
+        private readonly Harmony harmony = new Harmony("KurisuBot.UIMineSound");
+        internal static List<AudioClip> UI;
+        internal static AssetBundle Bundle;
+        internal static string text;
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            text = instance.Info.Location.TrimEnd("UIMineSound.dll".ToCharArray());
+            mls = BepInEx.Logging.Logger.CreateLogSource("KurisuBot.UIMineSound");
+            UIMineSound.UI = new List<AudioClip>();
+            UIMineSound.Bundle = AssetBundle.LoadFromFile(UIMineSound.text + "ui");
+            if (UIMineSound.Bundle != null)
+            {
+                UIMineSound.UI = UIMineSound.Bundle.LoadAllAssets<AudioClip>().ToList();
+            }
+            mls.LogInfo("UIMineSound Loading. . .");
+            UIMineSound.silenceMines = base.Config.Bind<bool>("UIMines", "SilenceMines", false, "Silence the passive mine beeping.");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+            mls.LogInfo("UIMineSound Loaded!!");
+        }
+        public static void Log(string message)
+        {
+            UIMineSound.instance.Logger.LogInfo(message);
+        }
+    }
+}
